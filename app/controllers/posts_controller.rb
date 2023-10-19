e88a81163d71daad5855a9845e1d82f1dcef6e23
Vinicius Libero <libero.moreira@gmail.com>
2023-10-18 21:06:04 -0300

    fixed edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update!(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def new
@@ -9,7 +22,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
+    if @post.save!
       redirect_to @post
     else
       render 'new'
@@ -17,29 +30,18 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to @post
-    else
-      render 'edit'
-    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path
   end
 
   private
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
 
     def post_params
       params.require(:post).permit(:title, :body)
